82493b0c1baa1169bc0857d26ee9aab63cecc4b7
Rodrigo Nogueira <rdg6design@gmail.com>
2019-11-24 09:53:25 -0300

    Api curso react

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageheader'
+
+export default props =>(
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa historia</h2>
+        <p>Rodrigo Nogueira</p>
+        <h2>missao</h2>
+        <p>Javascript</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum</p>
+    </div>
+)
